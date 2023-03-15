2aaca0f0f095b8c23daf2bb7da2788e2707a6d5d
Noman <80441451+nali556@users.noreply.github.com>
2023-03-15 11:49:22 -0400

    called api in book component

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,9 +1,27 @@
 import Container from '../components/Container';
 import {useParams} from 'react-router-dom';
+import { useEffect } from 'react';
 
 const Book = () => {
-
     const params = useParams();
+    console.log(params)
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const getData = async () => {
+        const url = 'https://api.matgargano.com/api/books/'+params.id;
+        try {
+            const request = await fetch(url);
+            const response = await request.json();
+            console.log(response)
+           
+        } catch(e) {
+            console.error(e.message);
+        } finally {
+        }
+    }
 
     return (<Container>
         <pre>{JSON.stringify(params,0,1)}</pre>
