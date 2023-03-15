e443a331a1e06db5dd1747145934e3d12a935091
Noman <80441451+nali556@users.noreply.github.com>
2023-03-15 13:11:38 -0400

    added spinnter

--- src/pages/Book.jsx
+++ src/pages/Book.jsx
@@ -1,30 +1,52 @@
 import Container from '../components/Container';
 import {useParams} from 'react-router-dom';
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
+import { Oval } from 'react-loader-spinner' 
 const Book = () => {
+    const [loading, setLoading] = useState(true); 
+    const [error, setError] = useState(false);
+    const [res, setRes] = useState({})
     const params = useParams();
-    console.log(params)
 
     useEffect(() => {
-        getData()
+        getData();
     }, [])
 
     const getData = async () => {
-        const url = 'https://api.matgargano.com/api/books/'+params.id;
+        const url = 'https://api.matgargano.com/api/books/'+ params.id;
         try {
             const request = await fetch(url);
             const response = await request.json();
-            console.log(response)
-           
+            console.log(response);
+            setRes(response)
         } catch(e) {
-            console.error(e.message);
+            setError('Error: ' + e.message);
         } finally {
+            setLoading(false);
         }
     }
 
     return (<Container>
-        <pre>{JSON.stringify(params,0,1)}</pre>
+        {!error && loading ? <div className="flex justify-center"><Oval height={600}/></div>:
+        <div className="container">
+            <div className="text-sm breadcrumb">Back to List</div>
+            <div className="flex flex-col items-center book-card">
+                <h1 className="text-3xl book-title">{res.title}</h1>
+                <p className="book-author text-x1">{res.author}</p>
+                <img src={res.imageURL} alt="bookimage"></img>
+                <div className="book-info flex flex-row justify-between">
+                    <p className="mr-3">Pages: {res.pages} pages</p>
+                    <p className="mr-3">Year Published: {res.year}</p>
+                </div>
+                    <div className="book-info flex flex-row justify-between">
+                    <p className="mr-3">Publisher: {res.publisher}</p>
+                    <p className="mr-3">Book ID: {res.id}</p>
+                </div>
+            </div>
+        </div>
+        
+        }
+        
     </Container>)
 }
 
